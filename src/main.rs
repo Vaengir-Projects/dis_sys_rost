fc8142b8bb9fc78610a900c51735e2f889943478
Weiberle17 <maxweiberle63@gmail.com>
2023-12-14 14:34:38 +0100

    feat: Starting backend

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,54 @@
-fn main() {
-  todo!();
+mod queries;
+mod structs;
+
+use crate::queries::{add_drink, add_food, get_all_drinks, get_all_food};
+use axum::{
+  body::Body,
+  http::Request,
+  middleware::{self, Next},
+  response::Response,
+  routing::{get, post},
+  Extension, Router,
+};
+use chrono::Local;
+use dotenv::dotenv;
+use sqlx::MySqlPool;
+use std::env;
+
+async fn logging_middleware(req: Request<Body>, next: Next<Body>) -> Response {
+  println!("Received a request to {} at: {}", req.uri(), Local::now());
+  next.run(req).await
+}
+
+#[tokio::main]
+async fn main() {
+  dotenv().ok();
+
+  let database_url = env::var("DATABASE_URL").expect("Can't find database url");
+  let pool = MySqlPool::connect(&database_url)
+    .await
+    .expect("Couldn't connect to the database");
+  let server_url = env::var("SERVER_URL").expect("Can't find server url");
+  let server_url_display =
+    env::var("SERVER_URL_DISPLAY").expect("Can't find server url to display");
+
+  let app = Router::new()
+    .route("/", get(|| async { "Gastronomie 9000" }))
+    .route("/food", get(get_all_food).layer(Extension(pool.clone())))
+    .route("/add/food", post(add_food).layer(Extension(pool.clone())))
+    .route(
+      "/drinks",
+      get(get_all_drinks).layer(Extension(pool.clone())),
+    )
+    .route(
+      "/add/drinks",
+      post(add_drink).layer(Extension(pool.clone())),
+    )
+    .layer(middleware::from_fn(logging_middleware));
+
+  println!("Running on http://{}", &server_url_display);
+  axum::Server::bind(&server_url.parse().unwrap())
+    .serve(app.into_make_service())
+    .await
+    .unwrap();
 }
---
+++ src/structs.rs
@@ -0,0 +1,51 @@
+use chrono::{DateTime, Local};
+use serde::{Deserialize, Serialize};
+use sqlx::FromRow;
+
+#[derive(Debug, Clone, Serialize, Deserialize, FromRow)]
+pub struct Tisch {
+  pub id: i64,
+  pub seats: i64,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, FromRow)]
+pub struct Food {
+  #[serde(default)]
+  pub id: i64,
+  pub name: String,
+  pub price: f64,
+  #[serde(default)]
+  pub vegetarian: bool,
+  #[serde(default)]
+  pub vegan: bool,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, FromRow)]
+pub struct Drinks {
+  #[serde(default)]
+  pub id: i64,
+  pub name: String,
+  pub price: f64,
+  pub size: f64,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, FromRow)]
+pub struct Order {
+  #[serde(default)]
+  pub id: i64,
+  pub order_timestamp: DateTime<Local>,
+  pub total_amount: f64,
+  #[serde(default)]
+  pub paid: bool,
+  #[serde(default)]
+  pub pay_timestamp: Option<DateTime<Local>>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, FromRow)]
+pub struct OrderItem {
+  #[serde(default)]
+  pub id: i64,
+  pub order_id: i64,
+  pub item_id: String,
+  pub quantity: i64,
+}
