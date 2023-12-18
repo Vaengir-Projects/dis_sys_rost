8f29137bcbc74960ada59deab7ab69121308bae0
Vaengir <vaengir@gmx.de>
2023-12-18 15:07:33 +0100

    refactor: To allow right database structure
    Could get a foreign key of varchar to work. So now everything uses
    int.

--- src/queries.rs
+++ src/queries.rs
@@ -2,92 +2,6 @@ use crate::structs::*;
 use axum::{http::StatusCode, response::IntoResponse, Extension, Json};
 use sqlx::MySqlPool;
 
-pub async fn food(Extension(pool): Extension<MySqlPool>) -> impl IntoResponse {
-  let query = "Select food_id, food_name, food_price from food".to_string();
-  let food: Vec<Food> = match sqlx::query_as(&query).fetch_all(&pool).await {
-    Ok(food) => food,
-    Err(e) => {
-      return (
-        StatusCode::INTERNAL_SERVER_ERROR,
-        format!("Internal server error: {}", e),
-      )
-        .into_response()
-    }
-  };
-
-  (StatusCode::OK, Json(food)).into_response()
-}
-
-pub async fn add_food(
-  Extension(pool): Extension<MySqlPool>,
-  Json(data): Json<Food>,
-) -> impl IntoResponse {
-  println!("{:?}", data);
-  let query = format!(
-    "Insert into food (food_name, food_price) values ('{}', {})",
-    data.food_name, data.food_price
-  );
-  let rows_affected = match sqlx::query(&query).execute(&pool).await {
-    Ok(rows_affected) => rows_affected,
-    Err(e) => {
-      return (
-        StatusCode::INTERNAL_SERVER_ERROR,
-        format!("Internal server error: {}", e),
-      )
-        .into_response()
-    }
-  };
-
-  (
-    StatusCode::OK,
-    format!("Number of rows affected: {:?}", rows_affected),
-  )
-    .into_response()
-}
-
-pub async fn drinks(Extension(pool): Extension<MySqlPool>) -> impl IntoResponse {
-  let query = "Select drink_id, drink_name, drink_price, drink_size from drinks".to_string();
-  let drinks: Vec<Drinks> = match sqlx::query_as(&query).fetch_all(&pool).await {
-    Ok(drinks) => drinks,
-    Err(e) => {
-      return (
-        StatusCode::INTERNAL_SERVER_ERROR,
-        format!("Internal server error: {}", e),
-      )
-        .into_response()
-    }
-  };
-
-  (StatusCode::OK, Json(drinks)).into_response()
-}
-
-pub async fn add_drink(
-  Extension(pool): Extension<MySqlPool>,
-  Json(data): Json<Drinks>,
-) -> impl IntoResponse {
-  println!("{:?}", data);
-  let query = format!(
-    "Insert into drinks (drink_name, drink_price, drink_size) values ('{}', {}, {})",
-    data.drink_name, data.drink_price, data.drink_size
-  );
-  let rows_affected = match sqlx::query(&query).execute(&pool).await {
-    Ok(rows_affected) => rows_affected,
-    Err(e) => {
-      return (
-        StatusCode::INTERNAL_SERVER_ERROR,
-        format!("Internal server error: {}", e),
-      )
-        .into_response()
-    }
-  };
-
-  (
-    StatusCode::OK,
-    format!("Number of rows affected: {:?}", rows_affected),
-  )
-    .into_response()
-}
-
 pub async fn items(Extension(pool): Extension<MySqlPool>) -> impl IntoResponse {
   let query = "Select item_id, item_type, item_name, item_price from items".to_string();
   let items: Vec<Items> = match sqlx::query_as(&query).fetch_all(&pool).await {
@@ -102,4 +16,31 @@ pub async fn items(Extension(pool): Extension<MySqlPool>) -> impl IntoResponse {
   };
 
   (StatusCode::OK, Json(items)).into_response()
+}
+
+pub async fn add_item(
+  Extension(pool): Extension<MySqlPool>,
+  Json(data): Json<Items>,
+) -> impl IntoResponse {
+  println!("{:?}", data);
+  let query = format!(
+    "Insert into items (item_name, item_price, item_type) values ('{}', {}, '{}')",
+    data.item_name, data.item_price, data.item_type
+  );
+  let rows_affected = match sqlx::query(&query).execute(&pool).await {
+    Ok(rows_affected) => rows_affected,
+    Err(e) => {
+      return (
+        StatusCode::INTERNAL_SERVER_ERROR,
+        format!("Internal server error: {}", e),
+      )
+        .into_response()
+    }
+  };
+
+  (
+    StatusCode::OK,
+    format!("Number of rows affected: {:?}", rows_affected),
+  )
+    .into_response()
 }
